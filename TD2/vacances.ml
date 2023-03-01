23fb0b2ff05d21c15878b2511c23ec2aef7c7ce8
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-03-01 17:00:52 +0100

    Done

--- TD2/vacances.ml
+++ TD2/vacances.ml
@@ -7,6 +7,19 @@ let rec somme liste = match liste with
 let rec mini l = match l with
 |[] -> failwith "none"
 |[a] -> a
-|t::q -> min t (mini q)
-let l = [8, 6, 7, 4, 2, 0]
+|t::q -> let m = mini q in if t < m then t else m
+let l = [8; 6; 7; 4; 2; 0; -2]
 let m = mini l;;
+
+(*Exo5*)
+let rec reverse liste = match liste with
+|[] -> []
+|t::[a] -> a::[t]
+|t::q -> (reverse q)@[t]
+
+let betterReverse liste =
+  let rec transfer reversed = function
+  |[] -> reversed
+  |t::q -> transfer (t::reversed) q
+  in transfer [] liste
+
