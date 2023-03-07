6dc85b403ab7cfd82759b7728050ba702c7dbdff
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-03-07 09:12:22 +0100

    Les vacances sont finies

--- TD1/vacances.ml
+++ TD1/vacances.ml
@@ -3,12 +3,12 @@
 let f = function
 |0 -> true
 |x when ((x - 2) mod 4) = 0 || ((x - 1) mod 2) = 0 -> true
-|_ -> false 
+|_ -> false;;
 
 (*Exo 8*)
 
-let curry f x y = f (x,y)
-let uncurry f (x,y) = f x y
+let curry f x y = f (x,y);;
+let uncurry f (x,y) = f x y;;
 
 (*Exo 10*)
 type ent = Zero | N of ent * ent
