58986aaa751cf68cb03a9ac0ad56ae4b8703fe97
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-03-22 17:54:12 +0100

    GO

--- TPs/TP1.ml
+++ TPs/TP1.ml
@@ -62,7 +62,3 @@ let decoupe_strict l p =
   |t::q -> if t < p then aux (t::li) lp np q else if t > p then aux li (t::lp) np q else aux li lp (np+1) q in
   aux [] [] 0 l
 
-let ieme l i = 
-  let aux x = x in l;;
-let cube x = x*x*x
-let armstrong n = let a = n mod 10 in let b = (n-a)/10 mod 10 in let c = (n - 10*b - a)/100 in n = (cube a) + (cube b) + (cube c)
