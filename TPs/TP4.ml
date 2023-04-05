823e0e7e83092156ecf8ecbee64266cafd54b775
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-04-05 19:04:57 +0200

    Tout marche

--- TPs/TP4.ml
+++ TPs/TP4.ml
@@ -6,6 +6,44 @@
 *)
 let deplace (i,j) = 
   let arrives = [] in (i+2, j-1)::(i+2, j+1)::(i-2, j-1)::(i-2, j+1)::(i-1, j-2)::(i-1, j+2)::(i+1, j-2)::(i+1, j+2)::arrives;;
-let valid (n, p) (x,y) = 
-  if 0<=x && x<n && 0<=y && y<p then true
-  else false;;
+let valide (n, p) (x,y) = 
+  0<=x && x<n && 0<=y && y<p;;
+
+let rec filter p li = match li with
+|[] -> []
+|t::q when p t -> t::(filter p q)
+|t::q -> filter p q;;
+
+let deplacements_valides (n,p) (x,y) = 
+  filter (valide (n,p)) (deplace (x,y));;
+let set matrix pos value =
+  let x = fst pos and y = snd pos in
+  matrix.(x).(y) <- value;;
+let get matrix pos = 
+  let x = fst pos and y = snd pos in
+  matrix.(x).(y);;
+let resoudre dimensions = 
+  let (n,p) = dimensions in
+  let echequier = Array.make_matrix n p true in
+  let successeur = Array.make_matrix n p (-1,-1) in
+  let rec parcours nb_parcourues prpos pos =
+    set successeur prpos pos;
+    if (nb_parcourues = (n*p) - 1) then true
+    else 
+      begin
+      set echequier pos false;
+      let d = deplacements_valides (n,p) pos in
+      if (List.exists (parcours (nb_parcourues +1) pos) (filter (fun coord -> get echequier coord) d)) 
+        then true
+      else begin
+          set echequier pos true;
+          false 
+          end
+      end
+  in (parcours 0 (0,0) (0,0), successeur);;
+
+  let rec chemin matrix pos = 
+
+
+
+
