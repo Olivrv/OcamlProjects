37ca1f781ff7a4f66924104cc79dfed0188b37c2
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-04-06 12:59:59 +0200

    Done

--- TD4/code.ml
+++ TD4/code.ml
@@ -8,8 +8,9 @@ let rec foo x n = match n with
 |0 -> 1
 |n -> x * (foo x (n-1));;
 
-let footerm x n = match n with
-|0 -> 1
-|1 -> x
-|n when n mod 2 = 0 -> foo (x*x) (n/2)
-|n -> foo (x*x) (n/2);;
+let footerm x n = 
+  let rec aux x n acc = match n with
+  |0 -> 1
+  |1 -> x * acc
+  |n -> aux x (n-1) (acc * x) 
+in aux x n 1;;
