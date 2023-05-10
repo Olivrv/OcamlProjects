4eee5d22bad1e09aaf751112fba10b721acadf01
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-05-10 18:23:41 +0200

    GOOOO

--- TPs/TP6.ml
+++ TPs/TP6.ml
@@ -1,6 +1,11 @@
-type 'a arbre = Noeud of 'a * ('a arbre list);;  
-type 'a arbreBino = Noeud of ('a * int) * ('a arbre list);;
-let rang = function
-|Noeud(_, r) -> r;;
-let priorite = function
-|Noeud(p, _) -> p;;
+type 'a arbreBino = Noeud of 'a * ('a arbreBino list) * int;;
+let rang (Noeud(_, _, r)) = r;;
+let priorite (Noeud(p, _, _)) = p;;
+let creerArbre c = Noeud(c, [], 0);;
+let rec fusionArbre arb1 arb2 = match arb1, arb2 with 
+|Noeud(cle1, liste1, r), Noeud(cle2, liste2, _) when cle1 <= cle2 -> Noeud(cle2, arb1::liste2, r+1)
+|Noeud(cle1, liste1, r), Noeud(cle2, liste2, _) -> Noeud(cle1, arb2::liste1, r+1);;
+
+type 'a tasBino = 'a arbreBino list;;
+let insererArbre tas arbre = match arbre with
+|Noeud(v, l, r) -> 
