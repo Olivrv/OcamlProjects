e59bbfa75aff359a7cf0d34f071a73234455fd46
Olivier <60824785+Olivrv@users.noreply.github.com>
2023-11-30 13:56:00 +0100

    error

--- sudoku.ml
+++ sudoku.ml
@@ -1,13 +1,17 @@
 let numero (i,j) = 
   i*9 + j;;
+
 let coordonnes n = 
   let i = n / 9 and j = n mod (9) in (i,j);;
+
 let meme_bloc (i, j) (k, l) = 
   (j / 3 ) = (l / 3) && (i / 3) = (k / 3);;
+
 let condition m n = match (n,m) with
-|(n,n) -> false
+|(x,y) when x=y -> false
 |_ -> let (i,j) = coordonnes m and (k,l) = coordonnes n in
     (i=k) || (j=l) || meme_bloc (i,j) (k,l);;
+
 let verification grille m v = 
   let ok = ref true in
   for k = 0 to 80 do
@@ -18,27 +22,40 @@ let verification grille m v =
         ok := ((!ok)||(valeur=v))
    done;
    !ok;;
+
 let case_vide grille = 
   let k = ref 0 and zero = ref false in 
-  while k <> 81 && not zero do
-    let (i,j) = coordonnes k in
+  while !k <> 81 && not !zero do
+    let (i,j) = coordonnes !k in
     if grille.(i).(j) = 0 then zero := true
     else k := !k + 1
   done;
   !k;;
-let rec sudoku grille =
-  let vide = case_vide grille in
-  let (i,j) = coordonnes vide in
-  if vide = 81 then 
-    failwith "NotFound"
-  else 
-    let vtest = ref 1 in
-    let notfound = ref true in
-    while vtest <= 9 && notfound do
-      if verification vide vtest then
-        grille.(i).(j) <- !vtest;
-        notfound := false;
-        sudoku grille
-      else vtest := !vtest + 1
+
+let sudoku grille =
+  let rec exploration grille = match (case_vide grille) with
+  |81 -> true
+  |n -> let (i,j) = coordonnes n in let b = ref true in
+    for v = 1 to 9 do
+      if (verification grille n v) && (!b) then 
+        grille.(i).(j) <- v;
+        if exploration grille then b := false 
     done;
-    if notfound then 
+    b := false; 
+    !b;
+  in exploration grille;
+  if (case_vide grille) = 81 then raise Not_found;;
+
+let grille = 
+  [|
+  [|2;5;0;0;3;0;9;0;1|];
+  [|0;1;0;0;0;4;0;0;0|];
+  [|4;0;7;0;0;0;2;0;8|];
+  [|0;0;5;2;0;0;0;0;0|];
+  [|0;0;0;0;9;8;1;0;0|];
+  [|0;4;0;0;0;3;0;0;0|];
+  [|0;0;0;3;6;0;0;7;2|];
+  [|0;7;0;0;0;0;0;0;3|];
+  [|9;0;3;0;0;0;6;0;4|]
+  |];;
+
