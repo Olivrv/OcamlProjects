929315308f173d873c8628964c9dc2d3b609a185
Olivier <60824785+Olivrv@users.noreply.github.com>
2024-02-06 09:30:27 +0100

    Go go go

--- graphes/main.ml
+++ graphes/main.ml
@@ -1,9 +1,24 @@
 let kruskal g w = 
   let n = Array.length g in 
   let t = Array.make n in [] in 
-  let rec aux l b = match
+  let rec aux l b = match 
     |[] -> []
     |(u,v)::q -> if not chemin t u v then ajout_arete t u v; aux q in
     aux(tri_arete q w)
 
 //TODO chemin, tri_arete
+  
+let rec chemin t u v = 
+  (*Parcours en profondeur*)
+  let n = Array.length t in
+	let visited = Array.make n false in
+	let p = Stack.create () in
+	stack.push r p;
+	while not in stack.is_empty p do
+		let u = stack.pop p in
+		if not visited.(u) then (
+			visited.(u) <- true;
+			List.iter (fun v -> stack.pool v p ) g.(u))
+	done;
+;;
+  
